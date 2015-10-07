9740cb0878b7db2b4569166b76bf5e6d56f7fc4c
Paul Laptop <lynks05@hotmail.com>
2015-10-07 13:56:54 +0200

    initial cpm bundle commit

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y curl
+curl -sSL https://get.docker.com/ | sh
+service docker start
+usermod -aG docker vagrant
+apt-get install -y libzmq3 libzmq3-dev
+

b15f83d3cb93d5a783dd415d7a9555f4cff8ed35
Paul Laptop <lynks05@hotmail.com>
2016-02-29 13:01:27 +0100

    ld

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # The base path of the installation (the place where the cpm git repository was cloned)
-export CPM_HOME=/vagrant 
+#export CPM_HOME=/vagrant 
+export CPM_HOME=/home/paul/projects/custom/cpm 
+
 
 # Paths of provided dependencies relative to the base path
 export JAVA_HOME=$CPM_HOME/lib/jdk1.8.0_51

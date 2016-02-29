171c66ab48b857bfbe208d3c20b04766e6d64bcd
Paul Bui-Quang <paul.bui-quang@limsi.fr>
2016-02-29 13:01:08 +0100

    minor root changes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,6 +10,9 @@ echo "Installing python client :"
 cd $CPM_HOME/cli
 pip install .
 ./generate-autocomplete.sh
-echo ". $CPM_HOME/cli/cpm-complete.sh" >> /home/vagrant/.bashrc
+echo ". $CPM_HOME/cli/cpm-complete.sh" >> $HOME/.bashrc
+
+echo "Installation complete."
+echo "To use AppFM CLI make sure $HOME/.local/bin is in you PATH and source your bashrc to enable autocompletion"
 
 

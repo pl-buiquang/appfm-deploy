5fcd80005f711af4d549acf8210138df00d2b34a
Paul Laptop <lynks05@hotmail.com>
2016-03-22 10:20:48 +0100

    updated submodules, added default config auto replace

---
+++ scripts/default_config/replace.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CURDIR=`dirname $0`
+
+cp $CURDIR/conf.yml $CURDIR/../../core/src/main/resources/conf.yml 
+cp $CURDIR/env.sh $CURDIR/../../scripts/env.sh 
+cp $CURDIR/start.sh $CURDIR/../../
+cp $CURDIR/settings.php $CURDIR/../../web/settings.php 
+cp $CURDIR/.htaccess $CURDIR/../../web/.htaccess
+
+
---
+++ scripts/default_config/settings.php
@@ -0,0 +1,22 @@
+<?php
+
+define("DEBUG",true);
+define("LOG_HOME_PATH",'log/');
+define("PAGE_MAIN_TEMPLATE" ,"templates/html.tpl.php");
+define("TITLE","CPM");
+define("APP_HOME_ROUTE_FIX","");
+define("BASE_DIR","/vagrant/web/");
+$hostname = "http://localhost:8080/";
+if(!empty($_SERVER["HTTP_HOST"])){
+  $hostname = $_SERVER['REQUEST_SCHEME']."://".$_SERVER["HTTP_HOST"].APP_HOME_ROUTE_FIX."/";
+}
+
+define("BASE_URL",$hostname);
+define("CALLER_DEFAULT_MAIN_TEMPLATE",'front_default_main_template');
+
+
+define("CPM_HOST","localhost");
+define("CPM_PORT","5555");
+
+define("CPM_WS_HOST","localhost:8083");
+

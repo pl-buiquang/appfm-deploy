3f229f4f5a9f4b4a4f1c136a4f7d1e5894f4efb9
Paul Laptop <lynks05@hotmail.com>
2016-06-29 23:59:27 +0200

    updated core and web

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -66,14 +66,15 @@ printf "[zmq]\nextension=zmq.so\n\n[mongodb]\nextension=mongo.so" >> /etc/php5/a
 service apache2 restart
 
 # additionnal disk for mongo & docker
+mkdir /mnt/disk
+# !!! warning here, untested code on many vagrant possible configuration !!!!
 apt-get install -y parted
 parted /dev/sdb mklabel msdos
 parted /dev/sdb mkpart primary 512 100%
 mkfs.ext4 /dev/sdb1
-mkdir /mnt/disk
 echo `blkid /dev/sdb1 | awk '{print$2}' | sed -e 's/"//g'` /mnt/disk   ext4   noatime,nobarrier   0   0 >> /etc/fstab
 mount /mnt/disk/
-
+# !!! end warning
 
 service mongodb stop
 service docker stop
